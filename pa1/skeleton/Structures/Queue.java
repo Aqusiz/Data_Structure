36e4e9d8d8d04d8832bb3af1b5aa419ed46e8cdd
Juno <kjo1797@gmail.com>
2023-04-07 14:41:26 +0900

    Implement Queue w/o exception

--- pa1/skeleton/Structures/Queue.java
+++ pa1/skeleton/Structures/Queue.java
@@ -1,29 +1,38 @@
 package Structures;
 
 public class Queue {
-    // FILL HERE
+    private int[] arr;
+    private int max_size;
+    int hd, tl;
 
     public Queue(int max_len){
-        // FILL HERE
+        max_size = max_len;
+        arr = new int[max_size + 1];
+        hd = 0;
+        tl = 0;
     }
 
     public void clear(){
-        // FILL HERE
+        hd = 0;
+        tl = 0;
     }
 
     public boolean isempty(){
-        // FILL HERE
+        return (tl == hd);
     }
 
     public int length(){
-        // FILL HERE
+        return tl - hd;
     }
 
     public void push(int val){
-        // FILL HERE
+        arr[tl++] = val;
+        if(tl == max_size) tl = 0;
     }
 
     public int pop(){
-        // FILL HERE
+        int ret = arr[hd++];
+        if(hd == max_size) hd = 0;
+        return ret;
     }
 }
