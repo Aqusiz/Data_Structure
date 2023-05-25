fe44908885c04f83f3f286fabeaad2ee0cf50098
Juno <kjo1797@gmail.com>
2023-05-25 17:34:17 +0900

    pa4

---
+++ pa4/PA04_skeleton/src/HashTable.java
@@ -0,0 +1,42 @@
+
+public class HashTable {
+    private int[] table;
+    private int c1, c2, c3;
+    private int delMarker = -1;
+    private int tableSize;
+
+    public HashTable(int n) {
+        // TODO: fill your code
+
+    }
+
+    public void create(int c1, int c2, int c3) {
+        // TODO: fill your code
+
+    }
+
+    public void insert(int key) {
+        // TODO: fill your code
+
+    }
+
+    public void delete(int key) {
+        // TODO: fill your code
+
+    }
+
+    public void search(int key) {
+        // TODO: fill your code
+
+    }
+
+    public void maxProbe() {
+        // TODO: fill your code
+
+    }
+
+    private int quadProb(int k, int i) {
+        // TODO: fill your code
+
+    }
+}
